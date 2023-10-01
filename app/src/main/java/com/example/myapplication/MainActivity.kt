7a1a8c9792c11053cd29af9574f2ff3b1acd7d6a
alchemija <juste.jagminaite@knf.stud.vu.lt>
2023-10-01 16:38:34 +0300

    added button + text

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClick(view: View) {
+
+        var displayText : TextView = findViewById(R.id.textHello)
+        displayText.setText("Hello World!")
+
     }
 }
