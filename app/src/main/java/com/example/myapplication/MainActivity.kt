40907b586460fc73c787e33c8e9006440ed2a0fe
alchemija <juste.jagminaite@knf.stud.vu.lt>
2023-10-01 17:19:15 +0300

    Revert "added button for changing color + comments"
    This reverts commit 0172e28aad5702a4d2e7d06d62ea450000f70106.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication
-import android.graphics.Color
-import android.widget.Button
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,21 +12,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun buttonClick(view: View) {
-        //text gets displayed on button press
+
         var displayText : TextView = findViewById(R.id.textHello)
         displayText.setText("Hello World!")
 
     }
-
-    fun toggleColor(view: View) {
-            val colorButton = view as Button
-        //possible colors defined
-            val colors = arrayOf(Color.RED, Color.GREEN, Color.BLUE)
-        //finds current color
-            val currentColor = colorButton.currentTextColor
-            val currentIndex = colors.indexOf(currentColor)
-        //changes color to next color in array
-            val nextIndex = (currentIndex + 1) % colors.size
-            colorButton.setTextColor(colors[nextIndex])
-        }
-    }
+}
